1cd62177de3be0c2378ea889a1ad405e97be0a0b
JamesKnight0001 <80499782+JamesKnight0001@users.noreply.github.com>
2022-10-25 07:08:44 -0700

    stop fucking dming me please

--- SPTS-STATUS.lua
+++ SPTS-STATUS.lua
@@ -68,7 +68,23 @@ Tab0:AddToggle({
 	Flag = "Turn On/Off webhook",
 	Default = toggle,
 	Callback = function(Value)
-		toggle = Value
+	    toggle = Value
+	    wait(0.5)
+	    if toggle == true and Webhook_URL == "" then
+	        OrionLib:MakeNotification({
+                Name = "idiot",
+                Content = "You forgot to add ur webhook idiot toggle me off and place ur webhook and toggle me on again",
+                Image = "rbxassetid://4483345998",
+                Time = 3
+            })
+        else
+            OrionLib:MakeNotification({
+                Name = "Webhook",
+	            Content = "Your Webhook is "..Webhook,
+	            Image = "rbxassetid://4483345998",
+	            Time = 3
+            })
+        end
 	end    
 })
 
@@ -85,7 +101,7 @@ Tab0:AddToggle({
 ---
 Tab0:AddParagraph("Credits: James ","Credits")
 Tab1:AddParagraph("adding this soon im fucking lazy","soon")
-Tab1:AddParagraph("also i dont know how to add save system i only fucking do python","")
+
 ---
 while true do wait(1)
 if toggle == true then seconds = seconds + 1
