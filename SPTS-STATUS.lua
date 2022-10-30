1f8406c9fce80b3e323822dec96f3875a2c84e61
JamesKnight0001 <80499782+JamesKnight0001@users.noreply.github.com>
2022-10-30 12:38:10 -0700

    Update SPTS-STATUS.lua

--- SPTS-STATUS.lua
+++ SPTS-STATUS.lua
@@ -29,8 +29,8 @@ local hours = 0
 local days = 0
 
 local delay = 1
-local Webhook = "" -- Put ur webhook here if u want to keep em safe
-local toggle = false -- on/off(to keep em safe)
+local Webhook = "https://discord.com/api/webhooks/1033274765827379202/8VLj5AJkn7l2CGcjHON3MHdRmZBHOhVWAC3FXvO_Zzy9DlTkVlH1VQdRlIm0hprVPtne" -- Put ur webhook here if u want to keep em safe
+local toggle = true -- on/off(to keep em safe)
 local inline = false
 --
 local Abr = function (n)
